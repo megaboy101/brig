68f9c7514cbba23b799dc0c848ce4372173d57c9
TaharkaZ <coolmojo1234@gmail.com>
2019-10-26 01:28:55 -0400

    Fixed dependencies for react-navigation. set up the stack navigator.

--- mobile-client/App.tsx
+++ mobile-client/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: () => <Text>Yo</Text>,
+  },
+});
+
+export default createAppContainer(AppNavigator)
 
 const styles = StyleSheet.create({
   container: {
